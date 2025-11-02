e91cdd00e5fd6ca8613f5b25ba2e17f715c9241f
ihkang <ihkang@ihkangui-MacBookPro.local>
2025-11-02 14:18:35 +0900

    first commit

---
+++ auth.c
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool check_password(char* username, char* password) {
+    if (strcmp(password, "admin123") == 0) return true;
+    return false;
+}
+
+bool login_user(char* username, char* password) {
+    if (check_password(username, password)) {
+        printf("Welcome %s!\n", username);
+        return true;
+    } else {
+        log_auth_failure(username);
+        return false;
+    }
+}
